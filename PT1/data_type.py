0eca72a3efa3440b9814b0f1bd8d3d4b386908eb
Shiseson <81512413+Shiseson@users.noreply.github.com>
2021-04-20 23:30:52 +0800

    Update data_type.py

--- PT1/data_type.py
+++ PT1/data_type.py
@@ -14,11 +14,11 @@ False
 # 有順序、不可動的列表　Tuple
 (3, 4, 5)
 ("Hello", "World")
-# 集合 Set
 # 集合 Set 無順序的資料集合
 {3, 4, 5}
 {"Hello", "World"}
-# 字典 Dictionary
+# 字典 Dictionary 鍵值對 (Key-Value Pair) 的集合
+
 {"apple":"蘋果", "data":"資料"}
 # 變數:用來來儲存資料的自訂名稱
 # 變數名稱=資料
@@ -30,5 +30,5 @@ print(x)
 # 反斜線可以用於程式過長 換行使用
 print\
     (y)
-# 反斜線也可用於取消其功能
+# 反斜線也可用於取消其功能 稱跳脫
 'I\'m come from chiayi.'

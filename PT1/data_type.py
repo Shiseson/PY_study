f07356efc339045ca2ef3a3c60a8ffe33cb57d1b
Shiseson <81512413+Shiseson@users.noreply.github.com>
2021-04-20 19:52:47 +0800

    Initial commit

---
+++ PT1/data_type.py
@@ -0,0 +1,33 @@
+# 資料:程式的基本單位_Variables and Types
+# 數字
+352456
+2.5
+# 字串
+"任意文字"
+"Hello World"
+# 布林值
+True
+False
+# 有順序、可動的列表 List 用逗號隔開
+[3, 4, 5]
+["Hello", "World"]
+# 有順序、不可動的列表　Tuple
+(3, 4, 5)
+("Hello", "World")
+# 集合 Set
+{3, 4, 5}
+{"Hello", "World"}
+# 字典 Dictionary
+{"apple":"蘋果", "data":"資料"}
+# 變數:用來來儲存資料的自訂名稱
+# 變數名稱=資料
+y=5
+# ;分號可作為分隔 句尾 但不建議因此使用 PYTHON推薦以迴車鍵代替分隔 也因本不需要;來當句尾 加上去是多餘的
+data=3 ; x=4
+# print(資料)
+print(x)
+# 反斜線可以用於程式過長 換行使用
+print\
+    (y)
+# 反斜線也可用於取消其功能
+'I\'m come from chiayi.'
---
+++ PT1/hello,world.py
@@ -0,0 +1,9 @@
+# In[1] Hello, World!
+print("This line will be printed.")
+# In[2] Indentation
+x = 1
+if x == 1:
+    # indented four spaces
+    print("x is 1.")
+
+# %%

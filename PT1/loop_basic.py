0cee937965f9c063e148dd50cba8f94b25bf880a
Shiseson <81512413+Shiseson@users.noreply.github.com>
2021-05-13 00:43:08 +0800

    Create loop_basic.py

---
+++ PT1/loop_basic.py
@@ -0,0 +1,20 @@
+# while 迴圈
+n=1
+while n<= 3:     #while 條件為布林值
+    print(n)
+    n+=1
+sum=0
+while n < 11 :
+    n+=1
+    sum+=n
+print(sum)
+# for 迴圈
+for x in [3, 5, 1]:
+    print(x)
+for y in "Hello" :
+    print(y)
+x, sum =map(int, [0, 0])
+for x in range(1, 11):
+    sum+=x
+print(sum)
+
